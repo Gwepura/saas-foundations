1520cb7fc716084fdd35744408af5c7910ceb76e
Gwepura <aketani32@gmail.com>
2024-12-24 19:17:32 +0200

    Prepare for railway

--- src/app/settings.py
+++ src/app/settings.py
@@ -25,8 +25,14 @@ SECRET_KEY = 'django-insecure-1378(h&ct!!lu&2dj@r@3o+z2%o+wb4k65wi@8#s=4o4tehz4f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if DEBUG:
+  ALLOWED_HOSTS += [
+    "127.0.0.1",
+    "localhost"
+  ]
 
 # Application definition
 
--- src/app/views.py
+++ src/app/views.py
@@ -15,3 +15,16 @@ def home_page_view(request, *args, **kwargs):
   PageVisit.objects.create(path=request.path)
 
   return render(request, html_template, my_context)
+
+def about_view(request, *args, **kwargs):
+  queryset = PageVisit.objects.filter(path=request.path)
+  my_title = "My Page"
+  my_context = {
+    "page_title": my_title,
+    "page_visit_count": queryset.count()
+  }
+
+  html_template = "home.html"
+  PageVisit.objects.create(path=request.path)
+
+  return render(request, html_template, my_context)
